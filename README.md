b1b90d6b65a3d208bba9b273a537aa907b3dffcb
Rob Beal <rob.beal@yotoplay.com>
2025-08-06 01:33:12 +0100

    chore: improve tests and fix readme errors

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ pyenv local
 # python environment
 python -m venv venv
 source venv/bin/activate
-pip install -r requirements-dev.txt
+pip install -r requirements.txt
 pytest
 ```
 
--- tests/test_cloudwatch_log_retention.py
+++ tests/test_cloudwatch_log_retention.py
@@ -20,7 +20,16 @@ def test_log_group_with_retention_30_days(checker):
     result = checker.scan_resource_conf(conf)
     assert result == CheckResult.PASSED
 
-def test_log_group_with_retention_less_than_30_days(checker):
+def test_log_group_with_retention_less_than_30_days_in_root(checker):
+    conf = {
+        'Type': 'AWS::Logs::LogGroup',
+        'RetentionInDays': limitInDays - 1
+    }
+
+    result = checker.scan_resource_conf(conf)
+    assert result == CheckResult.PASSED
+
+def test_log_group_with_retention_less_than_30_days_in_properties(checker):
     conf = {
         'Type': 'AWS::Logs::LogGroup',
         'Properties': {
@@ -42,9 +51,22 @@ def test_log_group_without_retention_policy(checker):
 
     result = checker.scan_resource_conf(conf)
     assert result == CheckResult.FAILED
-    assert checker.failure_reason == "RetentionInDays should be set to 30 days or less (to reduce cost, and avoid deletion request periods)"
+    assert checker.failure_reason.startswith("RetentionInDays should be set to 30 days or less")
 
-def test_log_group_with_retention_greater_than_30_days(checker):
+def test_log_group_with_retention_greater_than_30_days_in_root(checker):
+    conf = {
+        'Type': 'AWS::Logs::LogGroup',
+        'RetentionInDays': limitInDays + 1,
+        'Properties': {
+            'LogGroupName': 'BadRetentionLogGroup'
+        }
+    }
+
+    result = checker.scan_resource_conf(conf)
+    assert result == CheckResult.FAILED
+    assert checker.failure_reason.startswith("RetentionInDays should be set to 30 days or less")
+
+def test_log_group_with_retention_greater_than_30_days_in_properties(checker):
     conf = {
         'Type': 'AWS::Logs::LogGroup',
         'Properties': {
@@ -55,4 +77,4 @@ def test_log_group_with_retention_greater_than_30_days(checker):
 
     result = checker.scan_resource_conf(conf)
     assert result == CheckResult.FAILED
-    assert checker.failure_reason == "RetentionInDays should be set to 30 days or less (to reduce cost, and avoid deletion request periods)"
+    assert checker.failure_reason.startswith("RetentionInDays should be set to 30 days or less")
